8a6660556d8635f702e92bddf72c4e3ef6d0dbc0
kimnoca <kimnoca@naver.com>
2025-03-17 15:17:57 +0900

    feat : init project

---
+++ src/main/java/io/quacker/QuackerApplication.java
@@ -0,0 +1,13 @@
+package io.quacker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/quacker/QuackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.quacker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
