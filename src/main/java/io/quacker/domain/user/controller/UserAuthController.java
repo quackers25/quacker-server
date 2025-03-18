e2153d45a4120a1b78ffb66b558db7e908871a1d
cupokki <nd1346@gmail.com>
2025-03-18 23:18:38 +0900

    feat : 임시 컨트롤러

--- src/main/java/io/quacker/domain/user/controller/UserAuthController.java
+++ src/main/java/io/quacker/domain/user/controller/UserAuthController.java
@@ -4,6 +4,7 @@ import io.quacker.domain.user.dto.UserCreateDto;
 import io.quacker.domain.user.dto.UserLoginDto;
 import io.quacker.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
