b5a716f080dfd08b4b6cf11006bed32c5788c7a2
Woo <rhrhghrhrhgh@ajou.ac.kr>
2025-03-19 18:11:39 +0900

    fix : LastModifiedDate 로 변경

--- src/main/java/io/quacker/common/entity/BaseEntity.java
+++ src/main/java/io/quacker/common/entity/BaseEntity.java
@@ -6,7 +6,7 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
@@ -17,6 +17,6 @@ public abstract class BaseEntity {
     @Column(updatable = false)
     private LocalDateTime createdAt;
 
-    @LastModifiedBy
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 }
