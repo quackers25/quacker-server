c889dae16de6b7539a32f9a5704b212bb4623027
cupokki <nd1346@gmail.com>
2025-03-19 17:48:55 +0900

    feat : userService.getCurrentUser(), 로그인된 사용자를 가져옵니다.

--- src/main/java/io/quacker/domain/user/dto/UserLoginDto.java
+++ src/main/java/io/quacker/domain/user/dto/UserLoginDto.java
@@ -3,7 +3,9 @@ package io.quacker.domain.user.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import lombok.Builder;
 
+@Builder
 public record UserLoginDto(
         @NotBlank
         @Email(regexp = "^(?=.*\\.).*$", message = "이메일 형식")
