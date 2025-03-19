38863e86451529fdd6fe5fb9adf218675b328dea
Woo <rhrhghrhrhgh@ajou.ac.kr>
2025-03-19 15:05:38 +0900

    조회&검색 최신순, 좋아요 수 정렬 기능 추가

--- src/main/java/io/quacker/domain/post/service/PostService.java
+++ src/main/java/io/quacker/domain/post/service/PostService.java
@@ -5,10 +5,10 @@ import io.quacker.domain.post.dto.PostDto;
 import java.util.List;
 
 public interface PostService {
-    List<PostDto> getAllPosts();
+    List<PostDto> getAllPosts(String sortBy);
     PostDto getPost(Long postId);
-    List<PostDto> getPostsByUserId(Long userId);
-    List<PostDto> searchPosts(String keyword);
+    List<PostDto> getPostsByUserId(Long userId, String sortBy);
+    List<PostDto> searchPosts(String keyword, String sortBy);
     PostDto addPost(String text, Long userId);
     PostDto repost(Long postId, Long userId);
     PostDto updatePost(Long postId, String newText);
