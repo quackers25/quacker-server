708371c8d8405b9d9ca8cff573082627f250fb47
Woo <rhrhghrhrhgh@ajou.ac.kr>
2025-03-19 15:14:12 +0900

    PostImage 도메인 구현

--- src/main/java/io/quacker/domain/postimage/dto/PostImageDto.java
+++ src/main/java/io/quacker/domain/postimage/dto/PostImageDto.java
@@ -1,25 +1,21 @@
 package io.quacker.domain.postimage.dto;
 
 import io.quacker.domain.postimage.entity.PostImage;
-import lombok.Builder;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-@Builder
 public record PostImageDto(
         Long id,
         String imageUrl
 ) {
     // 단일 PostImage 엔티티 → PostImageDto 변환
     public static PostImageDto fromEntity(PostImage postImage) {
-        return PostImageDto.builder()
-                .id(postImage.getId())
-                .imageUrl(postImage.getImageUrl())
-                .build();
+        return new PostImageDto(postImage.getId(), postImage.getImageUrl());
     }
 
     // 리스트 변환 (List<PostImage> → List<PostImageDto>)
     public static List<PostImageDto> fromEntities(List<PostImage> postImages) {
-        return postImages.stream().map(PostImageDto::fromEntity).toList();
+        return postImages.stream().map(PostImageDto::fromEntity).collect(Collectors.toList());
     }
 }
