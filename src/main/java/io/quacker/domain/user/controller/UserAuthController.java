9840f0339505ff67604ab4c25f38f215d837a85e
cupokki <nd1346@gmail.com>
2025-03-19 11:03:53 +0900

    feat : Spring validation 적용

--- src/main/java/io/quacker/domain/user/controller/UserAuthController.java
+++ src/main/java/io/quacker/domain/user/controller/UserAuthController.java
@@ -3,11 +3,16 @@ package io.quacker.domain.user.controller;
 import io.quacker.domain.user.dto.UserCreateDto;
 import io.quacker.domain.user.dto.UserLoginDto;
 import io.quacker.domain.user.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @RequiredArgsConstructor
 @RestController
@@ -18,11 +23,12 @@ public class UserAuthController {
 
     /**
      * 로그인 엔드포인트
-     * @param userLoginDto
-     * @return
      */
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody UserLoginDto userLoginDto) {
+    public ResponseEntity<?> login(@RequestBody @Valid UserLoginDto userLoginDto, Errors errors) {
+        if (errors.hasErrors()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors.toString());
+        }
 //        userService.login(userCreateDto);
         return ResponseEntity.status(HttpStatus.OK).body(userService.login(userLoginDto));
     }
@@ -33,7 +39,10 @@ public class UserAuthController {
      * @return JWT 토큰 반환
      */
     @PostMapping("/join")
-    public ResponseEntity<?> join(@RequestBody UserCreateDto userCreateDto) {
+    public ResponseEntity<?> join(@RequestBody @Valid UserCreateDto userCreateDto, Errors errors) {
+        if (errors.hasErrors()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors.toString());
+        }
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.join(userCreateDto));
     }
 }
--- src/main/java/io/quacker/domain/user/dto/UserCreateDto.java
+++ src/main/java/io/quacker/domain/user/dto/UserCreateDto.java
@@ -1,15 +1,34 @@
 package io.quacker.domain.user.dto;
 
-import io.quacker.domain.user.entity.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 
 @Builder
 public record UserCreateDto (
+
+        @NotBlank
+        @Email(regexp = "^(?=.*\\.).*$")
         String email,
+
+        @Pattern(regexp = "(?=.*\\p{Ll})(?=.*\\p{Lu})(?=.*\\p{Nd})(?=.*[!@#$%^&*(),.?\":{}|<>])" +
+                "[\\p{L}\\p{Nd}!@#$%^&*(),.?\":{}|<>]{8,16}",
+                message = "비밀번호는 8~16자 사이여야 하며, 대문자, 소문자, 숫자, 특수문자를 포함해야 합니다.")
         String password,
+
+        @Pattern(regexp = "(?=.*\\p{Ll})(?=.*\\p{Lu})(?=.*\\p{Nd})(?=.*[!@#$%^&*(),.?\":{}|<>])" +
+                "[\\p{L}\\p{Nd}!@#$%^&*(),.?\":{}|<>]{8,16}",
+                message = "비밀번호는 8~16자 사이여야 하며, 대문자, 소문자, 숫자, 특수문자를 포함해야 합니다.")
         String confirmPassword,
+
+        @Pattern(regexp = "^[\\p{L}0-9]{2,16}$")
         String name,
+
         String bio,
+
+//        @Pattern(regexp = "")
         String avatarImageUrl,
+
         boolean isPrivate
 ) {}
