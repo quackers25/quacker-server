c0d06e94aa6caa00efb3f37e1314c87e9cdc0658
Woo <rhrhghrhrhgh@ajou.ac.kr>
2025-03-19 15:05:38 +0900

    조회&검색 최신순, 좋아요 수 정렬 기능 추가

--- src/main/java/io/quacker/domain/post/dao/PostRepository.java
+++ src/main/java/io/quacker/domain/post/dao/PostRepository.java
@@ -2,16 +2,18 @@ package io.quacker.domain.post.dao;
 
 import io.quacker.domain.post.entity.Post;
 import io.quacker.domain.user.entity.User;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+    // 모든 게시물 조회
+    List<Post> findAll(Sort sort);
+
     // 특정 사용자의 게시글 조회
-    List<Post> findByUser(User user);
+    List<Post> findByUser(User user, Sort sort);
 
     // 특정 원본 게시글의 리트윗(공유) 목록 조회
     List<Post> findByOriginPost(Post originPost);
@@ -20,5 +22,5 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findByUserOrderByCreatedAtDesc(User user);
 
     // 특정 키워드가 포함된 게시글 찾기 (대소문자 구분 없이 키워드 검색)
-    List<Post> findByTextContainingIgnoreCase(String keyword);
+    List<Post> findByTextContainingIgnoreCase(String keyword, Sort sort);
 }
