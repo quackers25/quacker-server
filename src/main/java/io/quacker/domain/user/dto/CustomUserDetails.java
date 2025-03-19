2fa4f615addf5ed687694eb36b162074f7a530ac
cupokki <nd1346@gmail.com>
2025-03-19 11:27:44 +0900

    feat : record -> class 로 변경

--- src/main/java/io/quacker/domain/user/dto/CustomUserDetails.java
+++ src/main/java/io/quacker/domain/user/dto/CustomUserDetails.java
@@ -1,16 +1,20 @@
 package io.quacker.domain.user.dto;
 
 import io.quacker.domain.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
 
-public record CustomUserDetails(
-        User user
-) implements UserDetails {
+@AllArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    @Getter
+    private User user;
+
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
