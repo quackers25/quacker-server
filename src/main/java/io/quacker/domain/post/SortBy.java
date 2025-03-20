a0193b4646902bdfab9ca4589b17ad53a1c4b8e1
Woo <rhrhghrhrhgh@ajou.ac.kr>
2025-03-20 16:15:46 +0900

    feat : 정렬 enum 추가

---
+++ src/main/java/io/quacker/domain/post/SortBy.java
@@ -0,0 +1,18 @@
+package io.quacker.domain.post;
+
+import org.springframework.data.domain.Sort;
+
+public enum SortBy {
+    NEWEST(Sort.by(Sort.Direction.DESC, "createdAt")),  // 최신순 정렬
+    LIKES(Sort.by(Sort.Direction.DESC, "likeCount"));   // 좋아요순 정렬
+
+    private final Sort sort;
+
+    SortBy(Sort sort) {
+        this.sort = sort;
+    }
+
+    public Sort getSort() {
+        return this.sort;
+    }
+}
