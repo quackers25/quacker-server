dc45761cf03bd590344c61fb945b00ef2b19ae7f
hangyeollee <ihangyeol890@gmail.com>
2025-03-21 18:48:45 +0900

    Postlike 수정 및 버그 해결

--- src/main/java/io/quacker/domain/postlike/entity/PostLike.java
+++ src/main/java/io/quacker/domain/postlike/entity/PostLike.java
@@ -1,47 +1,41 @@
 package io.quacker.domain.postlike.entity;
 
-import io.quacker.common.entity.BaseEntity;
 import io.quacker.domain.post.entity.Post;
 import io.quacker.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "post_likes")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(uniqueConstraints = {
-    @UniqueConstraint(columnNames = {"user_id", "post_id"})
-})
-public class PostLike extends BaseEntity {
+public class PostLike {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User user;
-
-    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id")
     private Post post;
 
-    private PostLike(User user, Post post) {
-        this.user = user;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Builder
+    public PostLike(Post post, User user) {
         this.post = post;
-    }
-
-    public static PostLike of(User user, Post post) {
-        PostLike postLike = new PostLike(user, post);
-        post.incrementLikeCount();
-        user.addLike(postLike);
-        return postLike;
-    }
-
-    public void unlike() {
-        this.post.decrementLikeCount();
-        this.user.removeLike(this);
+        this.user = user;
     }
 }
