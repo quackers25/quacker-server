17235913cb469397af91419ca7ceee21f7333b06
hangyeollee <ihangyeol890@gmail.com>
2025-03-21 18:48:45 +0900

    Postlike 수정 및 버그 해결

--- src/main/java/io/quacker/domain/user/dto/UserCreateDto.java
+++ src/main/java/io/quacker/domain/user/dto/UserCreateDto.java
@@ -30,5 +30,9 @@ public record UserCreateDto (
 //        @Pattern(regexp = "")
         String avatarImageUrl,
 
-        boolean isPrivate
+        boolean isPrivate,
+
+        @NotBlank
+        @Pattern(regexp = "^[\\p{L}0-9]{2,16}$")
+        String nickname
 ) {}
