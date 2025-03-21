788db78fefad1ed597b587ac5fb68c8dbc81d60d
Woo <rhrhghrhrhgh@ajou.ac.kr>
2025-03-21 10:50:46 +0900

    fix : Dto 최적화

--- src/main/java/io/quacker/domain/post/dto/PostDto.java
+++ src/main/java/io/quacker/domain/post/dto/PostDto.java
@@ -5,7 +5,6 @@ import io.quacker.domain.user.dto.UserDto;
 import lombok.Builder;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Builder
 public record PostDto(
@@ -15,11 +14,6 @@ public record PostDto(
         int repostCount,
         UserDto user,
         PostDto originPost // 리트윗, 공유된 원본 글
-//        List<CommentDto> comments,
-//        List<PostImageDto> postImages,
-//        List<PostMentionDto> postMentions,
-//        List<HashtagPostDto> hashtagPosts,
-//        List<PostLikeDto> likes
 ) {
     // Post 엔티티를 PostDto로 변환하는 정적 팩토리 메서드
     public static PostDto from(Post post) {
@@ -30,16 +24,11 @@ public record PostDto(
                 .repostCount(post.getRepostCount())
                 .user(UserDto.from(post.getUser()))
                 .originPost(post.getOriginPost() != null ? PostDto.from(post.getOriginPost()) : null)
-//                .comments(post.getComments().stream().map(CommentDto::from).toList())
-//                .postImages(post.getPostImages().stream().map(PostImageDto::from).toList())
-//                .postMentions(post.getPostMentions().stream().map(PostMentionDto::from).toList())
-//                .hashtagPosts(post.getHashtagPosts().stream().map(HashtagPostDto::from).toList())
-//                .likes(post.getLikes().stream().map(PostLikeDto::from).toList())
                 .build();
     }
 
     // List<Post>를 List<PostDto>로 변환하는 정적 팩토리 메서드
     public static List<PostDto> of(List<Post> posts) {
-        return posts.stream().map(PostDto::from).collect(Collectors.toList());
+        return posts.stream().map(PostDto::from).toList();
     }
 }
