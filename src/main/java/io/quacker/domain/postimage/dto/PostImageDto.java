65148290c16e21ba5e5ebf43fca0855a72e2f13d
Woo <rhrhghrhrhgh@ajou.ac.kr>
2025-03-21 09:11:12 +0900

    fix : @Builder로 변경

--- src/main/java/io/quacker/domain/postimage/dto/PostImageDto.java
+++ src/main/java/io/quacker/domain/postimage/dto/PostImageDto.java
@@ -1,16 +1,21 @@
 package io.quacker.domain.postimage.dto;
 
 import io.quacker.domain.postimage.entity.PostImage;
+import lombok.Builder;
 
 import java.util.List;
 
+@Builder
 public record PostImageDto(
         Long id,
         String imageUrl
 ) {
     // 단일 PostImage 엔티티 → PostImageDto 변환
     public static PostImageDto fromEntity(PostImage postImage) {
-        return new PostImageDto(postImage.getId(), postImage.getImageUrl());
+        return PostImageDto.builder()
+                .id(postImage.getId())
+                .imageUrl(postImage.getImageUrl())
+                .build();
     }
 
     // 리스트 변환 (List<PostImage> → List<PostImageDto>)
