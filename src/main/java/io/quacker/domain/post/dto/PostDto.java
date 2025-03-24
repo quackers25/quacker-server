1e6b96240a7fb3ab9c3a29691a1814a61903eb3a
Woo <rhrhghrhrhgh@ajou.ac.kr>
2025-03-24 16:42:09 +0900

    feat : 이미지 추가 기능 추가

--- src/main/java/io/quacker/domain/post/dto/PostDto.java
+++ src/main/java/io/quacker/domain/post/dto/PostDto.java
@@ -1,6 +1,7 @@
 package io.quacker.domain.post.dto;
 
 import io.quacker.domain.post.entity.Post;
+import io.quacker.domain.postimage.dto.PostImageDto;
 import io.quacker.domain.user.dto.UserDto;
 import lombok.Builder;
 
@@ -13,8 +14,8 @@ public record PostDto(
         int likeCount,
         int repostCount,
         UserDto user,
-        PostDto originPost // 리트윗, 공유된 원본 글
-//        List<PostImageDto> images
+        PostDto originPost, // 리트윗, 공유된 원본 글
+        List<PostImageDto> images
 ) {
     // Post 엔티티를 PostDto로 변환하는 정적 팩토리 메서드
     public static PostDto from(Post post) {
@@ -25,7 +26,7 @@ public record PostDto(
                 .repostCount(post.getRepostCount())
                 .user(UserDto.from(post.getUser()))
                 .originPost(post.getOriginPost() != null ? PostDto.from(post.getOriginPost()) : null)
-//               .images(PostImageDto.fromEntities(post.getPostImages()))
+                .images(PostImageDto.fromEntities(post.getPostImages()))
                 .build();
     }
 
@@ -39,7 +40,7 @@ public record PostDto(
                 .text("")
                 .likeCount(0)
                 .repostCount(0)
-//                .images(List.of()) // 이미지 리스트도 비워두기
+                .images(List.of()) // 이미지 리스트도 비워두기
                 .build();
     }
 }
