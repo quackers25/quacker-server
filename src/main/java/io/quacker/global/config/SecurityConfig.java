fb5cfd363f29f436ad3b3ec5389ce6337052954b
kimnoca <kimnoca@naver.com>
2025-03-25 21:49:07 +0900

    feat : Swagger 기본 설정 구성

--- src/main/java/io/quacker/global/config/SecurityConfig.java
+++ src/main/java/io/quacker/global/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,6 +20,9 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private static final String[] WITHE_LIST =
+        {"/h2-console/**", "/api/v1/auth/login", "/api/v1/auth/join", "/api/v1/auth/refresh" , "/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**"};
+
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     // BCrypt 인코더 추가
@@ -29,20 +33,24 @@ public class SecurityConfig {
 
     // 시큐리티 체인 설정
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthenticationProvider jwtTokenAuthenticationProvider) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http,
+                                           JwtAuthenticationProvider jwtTokenAuthenticationProvider)
+        throws Exception {
         return http
-                .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()))  // iframe 허용하기 위하여
-                .authorizeHttpRequests(auth -> auth
-                                .requestMatchers("/h2-console/**").permitAll() // H2 Console 접근 허용                                .anyRequest().authenticated()
-                                .requestMatchers("/api/v1/auth/login", "/api/v1/auth/join", "/api/v1/auth/refresh").permitAll()
-                                .requestMatchers("/api/v1/auth/logout").authenticated()
-                                .anyRequest().authenticated()
+            .csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .headers(headers -> headers.frameOptions(FrameOptionsConfig::sameOrigin))  // iframe 허용하기 위하여
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers(WITHE_LIST)
+                    .permitAll()
+                    .requestMatchers("/api/v1/auth/logout").authenticated()
+                    .anyRequest().authenticated()
 //                                .anyRequest().permitAll()
-                )
-                .authenticationProvider(jwtTokenAuthenticationProvider)
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .build();
+            )
+            .authenticationProvider(jwtTokenAuthenticationProvider)
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+            .build();
     }
+
+
 }
