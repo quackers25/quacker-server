fdb2e003bbefbc6c6f26167301c13926c518ec34
hangyeollee <ihangyeol890@gmail.com>
2025-03-26 16:42:45 +0900

    hashtag 기능 구현

--- src/main/java/io/quacker/domain/hashtag/entity/Hashtag.java
+++ src/main/java/io/quacker/domain/hashtag/entity/Hashtag.java
@@ -1,46 +1,66 @@
 package io.quacker.domain.hashtag.entity;
 
-import io.quacker.common.entity.BaseEntity;
 import io.quacker.domain.hashtagpost.entity.HashtagPost;
+import io.quacker.domain.post.entity.Post;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Table(name = "hashtags")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Hashtag extends BaseEntity {
-
+public class Hashtag {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true)
+    @Column(nullable = false, unique = true)
     private String name;
 
-    private int recentPostCount;
+    @Column(name = "post_count")
+    private int postCount = 0;
 
-    @OneToMany(mappedBy = "hashtag", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "hashtag", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<HashtagPost> hashtagPosts = new ArrayList<>();
 
-    private Hashtag(String name) {
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    public Hashtag(String name) {
         this.name = name;
-        this.recentPostCount = 0;
     }
 
-    public static Hashtag of(String name) {
-        return new Hashtag(name);
+    public void incrementPostCount() {
+        this.postCount++;
     }
 
-    public void incrementRecentPostCount() {
-        this.recentPostCount++;
+    public void decrementPostCount() {
+        if (this.postCount > 0) {
+            this.postCount--;
+        }
     }
 
     public void addHashtagPost(HashtagPost hashtagPost) {
         this.hashtagPosts.add(hashtagPost);
+        incrementPostCount();
+    }
+
+    public void removeHashtagPost(HashtagPost hashtagPost) {
+        if (this.hashtagPosts.remove(hashtagPost)) {
+            decrementPostCount();
+        }
     }
 }
