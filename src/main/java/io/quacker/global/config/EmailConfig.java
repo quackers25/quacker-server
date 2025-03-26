5d0558e667779647ea593c073ce5a5dd37234890
cupokki <nd1346@gmail.com>
2025-03-26 15:46:15 +0900

    feat : 이메일서비스 Stub

--- src/main/java/io/quacker/global/config/EmailConfig.java
+++ src/main/java/io/quacker/global/config/EmailConfig.java
@@ -3,7 +3,7 @@ package io.quacker.global.config;
 import io.quacker.common.dao.EmailCodeRepository;
 import io.quacker.domain.user.service.EmailCodeService;
 import io.quacker.domain.user.service.EmailCodeServiceImpl;
-import io.quacker.domain.user.service.EmailServiceStub;
+import io.quacker.domain.user.service.EmailCodeServiceStub;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -17,17 +17,19 @@ public class EmailConfig {
     private final JavaMailSender mailSender;
     private final EmailCodeRepository emailCodeRepository;
 
-//    @Bean
-//    public EmailCodeService emailService() {
-//        return new EmailServiceStub();
-//    }
-
     @Bean
-    public EmailCodeService emailCodeService(
-            JavaMailSender mailSender,
+    public EmailCodeService emailService(
             EmailCodeRepository emailCodeRepository,
-            @Value("${email-code.expiration}") Long CODE_EXPIRATION_TIME
-    ) {
-        return new EmailCodeServiceImpl(mailSender, emailCodeRepository, CODE_EXPIRATION_TIME);
+            @Value("${email-code.expiration}") Long CODE_EXPIRATION_TIME) {
+        return new EmailCodeServiceStub(emailCodeRepository, CODE_EXPIRATION_TIME);
     }
+
+//    @Bean
+//    public EmailCodeService emailCodeService(
+//            JavaMailSender mailSender,
+//            EmailCodeRepository emailCodeRepository,
+//            @Value("${email-code.expiration}") Long CODE_EXPIRATION_TIME
+//    ) {
+//        return new EmailCodeServiceImpl(mailSender, emailCodeRepository, CODE_EXPIRATION_TIME);
+//    }
 }
