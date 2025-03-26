d997096b4959eb811bcd48ec11f2826f88d3a887
Woo <rhrhghrhrhgh@ajou.ac.kr>
2025-03-26 15:48:44 +0900

    feat : swagger 문서화

--- src/main/java/io/quacker/domain/postimage/dto/PostImageDto.java
+++ src/main/java/io/quacker/domain/postimage/dto/PostImageDto.java
@@ -1,14 +1,20 @@
 package io.quacker.domain.postimage.dto;
 
 import io.quacker.domain.postimage.entity.PostImage;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 import java.util.List;
 
 @Builder
 public record PostImageDto(
+
+        @Schema(description = "이미지 ID", example = "1")
         Long id,
+
+        @Schema(description = "S3에 저장된 이미지 URL", example = "https://s3.amazonaws.com/your-bucket/image.jpg")
         String imageUrl
+
 ) {
     // 단일 PostImage 엔티티 → PostImageDto 변환
     public static PostImageDto fromEntity(PostImage postImage) {
