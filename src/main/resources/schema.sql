56fd4a4236cd7231129a09b6f9c46d863155877c
hangyeollee <ihangyeol890@gmail.com>
2025-03-28 14:15:47 +0900

    fix: 해시태그 카운트 중복 증가 문제 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,7 +6,6 @@ CREATE TABLE users (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    nickname VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
     bio TEXT,
     avatar_image_url VARCHAR(255),
